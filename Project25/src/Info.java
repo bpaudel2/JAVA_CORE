e1900e3f84e299c7889795cd6880d8167dc9837f
bpaudel2 <beekpal@gmail.com>
2019-03-30 17:21:26 -0500

    Added more files

---
+++ Project25/src/Info.java
@@ -0,0 +1,15 @@
+// We create interface whenever we see that there are two 
+// or more classes that will have same method
+// implementing class has to implement all of the methods of interface
+/*
+ * Class can implements more than one interfaces.
+ * Class can only extends one superclass.
+ * You can use interface everywhere else except while
+ * creating new object (You have to pass class name.)
+ */
+
+public interface Info {
+	// interface does not have method code
+	public void showInfo();
+
+}
