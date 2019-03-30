9734d182689423317eaf96b55e6cbaf88b5bd1ce
bpaudel2 <beekpal@gmail.com>
2019-03-30 18:47:17 -0500

    minor

--- AccessModifier/src/App.java
+++ AccessModifier/src/App.java
@@ -11,7 +11,7 @@ public class App {
 	public static void main(String[] args) {
 		Plant plant = new Plant();
 		System.out.println(plant.name);
-		System.out.println(plant.ID);
+		System.out.println(Plant.ID);
 		
 		// Won't work --- type is private
         //System.out.println(plant.type);
